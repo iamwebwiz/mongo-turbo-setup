885f07c336ffe3b100aaf408f94ffbf4a88a3df8
Ezekiel <iamwebwiz@gmail.com>
2020-06-05 07:05:45 +0100

    Update index/web routes

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,26 @@ const router = express.Router()
 	Data is rendered using the Mustache templating engine. For more
 	information, view here: https://mustache.github.io/#demo */
 router.get('/', (req, res) => {
-	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
+    res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
 /*  This route render json data */
 router.get('/json', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		app: process.env.TURBO_APP_ID,
-		data: 'this is a sample json route.'
-	})
+    res.json({
+        confirmation: 'success',
+        app: process.env.TURBO_APP_ID,
+        data: 'this is a sample json route.',
+    })
 })
 
 /*  This route sends text back as plain text. */
 router.get('/send', (req, res) => {
-	res.send('This is the Send Route')
+    res.send('This is the Send Route')
 })
 
 /*  This route redirects requests to Turbo360. */
 router.get('/redirect', (req, res) => {
-	res.redirect('https://www.turbo360.co/landing')
+    res.redirect('https://www.turbo360.co/landing')
 })
 
 
