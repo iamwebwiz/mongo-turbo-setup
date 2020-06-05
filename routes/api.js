146a7580c7155b30680f8a74d81aafc3ec1907dd
Ezekiel <iamwebwiz@gmail.com>
2020-06-05 07:36:30 +0100

    Update team model in api endpoints

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const Profile = require('../models/Profile')
+const Team = require('../models/Team')
 
+// Profiles-specific endpoints
 router.get('/profiles', (req, res) => {
     let filters = req.query
 
@@ -50,6 +52,57 @@ router.post('/profiles', (req, res) => {
         res.json({
             confirmation: 'success',
             profile: profile,
+        })
+    }).catch(err => {
+        res.json({
+            confirmation: 'failure',
+            message: err.message,
+        })
+    })
+})
+
+// Teams-specific endpoints
+router.get('/teams', (req, res) => {
+    let filters = req.query
+
+    Team.find(filters)
+    .then((teams) => {
+        res.json({
+            confirmation: 'success',
+            data: teams,
+        })
+    })
+    .catch((err) => {
+        res.json({
+            confirmation: 'failure',
+            message: err.message,
+        })
+    })
+})
+
+router.get('/teams/:id', (req, res) => {
+    const id = req.params.id
+
+    Team.findById(id)
+    .then((team) => {
+        res.json({
+            confirmation: 'success',
+            data: team,
+        })
+    })
+    .catch(() => {
+        res.json({
+            confirmation: 'failure',
+            data: `Unable to locate team with id: [${id}]`,
+        })
+    })
+})
+
+router.post('/teams', (req, res) => {
+    Team.create(req.body).then(team => {
+        res.json({
+            confirmation: 'success',
+            profile: team,
         })
     }).catch(err => {
         res.json({
