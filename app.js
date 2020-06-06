72046700a75cc646d3050d490af614d02f524276
Ezekiel <iamwebwiz@gmail.com>
2020-06-06 11:07:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Full Documentation - https://docs.turbo360.co
-const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID });
-const express = require('express');
+const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
+const express = require('express')
 
-const app = express();
+const app = express()
 
 const config = {
   views: 'views',
@@ -11,24 +11,24 @@ const config = {
   db: {
     url: 'mongodb://localhost/mongo-turbo-proj',
     type: 'mongo',
-    onError: (err) => {
-      console.log('DB connection failed!');
+    onError: err => {
+      console.log('DB connection failed!')
     },
     onSuccess: () => {
-      console.log('DB connection successful!');
+      console.log('DB connection successful!')
     },
   },
-};
+}
 
-vertex.configureApp(app, config);
-app.use(vertex.setContext(process.env));
+vertex.configureApp(app, config)
+app.use(vertex.setContext(process.env))
 
 // import routes
-const index = require('./routes/index');
-const api = require('./routes/api');
+const index = require('./routes/index')
+const api = require('./routes/api')
 
 // set routes
-app.use('/', index);
-app.use('/api', api); // sample API Routes
+app.use('/', index)
+app.use('/api', api) // sample API Routes
 
-module.exports = app;
+module.exports = app
