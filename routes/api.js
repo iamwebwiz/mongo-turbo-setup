adc8d740dc26767f992fff4b84bbbe19f6fddb5b
Ezekiel <iamwebwiz@gmail.com>
2020-06-09 23:21:55 +0100

    Reformat route files

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Profile = require('../models/Profile')
-const Team = require('../models/Team')
+const Profile = require('../models/Profile');
+const Team = require('../models/Team');
 
 // Profiles-specific endpoints
 router.get('/profiles', (req, res) => {
-  let filters = req.query
+  let filters = req.query;
 
   if (req.query.age != null) {
     filters = {
       age: {$gte: filters.age},
-    }
+    };
   }
 
   Profile.find(filters)
@@ -19,33 +19,33 @@ router.get('/profiles', (req, res) => {
       res.json({
         confirmation: 'success',
         data: profiles,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/profiles/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Profile.findById(id)
     .then(profile => {
       res.json({
         confirmation: 'success',
         data: profile,
-      })
+      });
     })
     .catch(() => {
       res.json({
         confirmation: 'failure',
         data: `Unable to locate profile with id: [${id}]`,
-      })
-    })
-})
+      });
+    });
+});
 
 router.post('/profiles', (req, res) => {
   Profile.create(req.body)
@@ -53,70 +53,70 @@ router.post('/profiles', (req, res) => {
       res.json({
         confirmation: 'success',
         profile: profile,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.put('/profiles/:id/update', (req, res) => {
-  const profileId = req.params.id
+  const profileId = req.params.id;
 
   Profile.findByIdAndUpdate(profileId, req.body, {new: true})
     .then(profile => {
       res.json({
         confirmation: 'success',
         data: profile,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 // Teams-specific endpoints
 router.get('/teams', (req, res) => {
-  let filters = req.query
+  let filters = req.query;
 
   Team.find(filters)
     .then(teams => {
       res.json({
         confirmation: 'success',
         data: teams,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/teams/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Team.findById(id)
     .then(team => {
       res.json({
         confirmation: 'success',
         data: team,
-      })
+      });
     })
     .catch(() => {
       res.json({
         confirmation: 'failure',
         data: `Unable to locate team with id: [${id}]`,
-      })
-    })
-})
+      });
+    });
+});
 
 router.post('/teams', (req, res) => {
   Team.create(req.body)
@@ -124,32 +124,32 @@ router.post('/teams', (req, res) => {
       res.json({
         confirmation: 'success',
         profile: team,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.put('/teams/:id/update', (req, res) => {
-  const teamId = req.params.id
+  const teamId = req.params.id;
 
   Team.findByIdAndUpdate(teamId, req.body, {new: true})
     .then(team => {
       res.json({
         confirmation: 'success',
         data: team,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Profile = require('../models/Profile')
-const Team = require('../models/Team')
+const Profile = require('../models/Profile');
+const Team = require('../models/Team');
 
 router.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
 router.get('/profiles', (req, res) => {
-  let filters = req.query
+  let filters = req.query;
 
   if (req.query.age != null) {
     filters = {
       age: {$gte: filters.age},
-    }
+    };
   }
 
   Profile.find(filters)
@@ -22,76 +22,76 @@ router.get('/profiles', (req, res) => {
       res.json({
         confirmation: 'success',
         data: profiles,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/profiles/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Profile.findById(id)
     .then(profile => {
       res.json({
         confirmation: 'success',
         data: profile,
-      })
+      });
     })
     .catch(() => {
       res.json({
         confirmation: 'failure',
         data: `Unable to locate profile with id: [${id}]`,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/teams', (req, res) => {
-  let filters = req.query
+  let filters = req.query;
 
   Team.find(filters)
     .then(teams => {
       res.json({
         confirmation: 'success',
         data: teams,
-      })
+      });
     })
     .catch(err => {
       res.json({
         confirmation: 'failure',
         message: err.message,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/teams/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Team.findById(id)
     .then(team => {
       res.json({
         confirmation: 'success',
         data: team,
-      })
+      });
     })
     .catch(() => {
       res.json({
         confirmation: 'failure',
         data: `Unable to locate team with id: [${id}]`,
-      })
-    })
-})
+      });
+    });
+});
 
 router.get('/add-team', (req, res) => {
-  res.render('add-team', {title: 'Add Team'})
-})
+  res.render('add-team', {title: 'Add Team'});
+});
 
 router.get('/add-profile', (req, res) => {
-  res.render('add-profile', {title: 'Add Profile'})
-})
+  res.render('add-profile', {title: 'Add Profile'});
+});
 
-module.exports = router
+module.exports = router;
