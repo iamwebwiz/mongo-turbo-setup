1cace6d61b65d0bb51e220b16c67fd2ec4bcafef
Ezekiel <iamwebwiz@gmail.com>
2020-06-11 00:47:54 +0100

    Update app.js with conditional statement for database uri

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 // Full Documentation - https://docs.turbo360.co
-const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
-const express = require('express')
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
+const express = require('express');
 
-const app = express()
+const app = express();
 
 const config = {
   views: 'views',
   static: 'public',
   logging: true,
   db: {
-    url: 'mongodb://localhost/mongo-turbo-proj',
+    url:
+      process.env.TURBO_ENV === 'dev'
+        ? process.env.MONGODB_URI
+        : process.env.PROD_MONGODB_URI,
     type: 'mongo',
     onError: err => {
-      console.log('DB connection failed!')
+      console.log('DB connection failed!');
     },
     onSuccess: () => {
-      console.log('DB connection successful!')
+      console.log('DB connection successful!');
     },
   },
-}
+};
 
-vertex.configureApp(app, config)
-app.use(vertex.setContext(process.env))
+vertex.configureApp(app, config);
+app.use(vertex.setContext(process.env));
 
 // import routes
-const index = require('./routes/index')
-const api = require('./routes/api')
+const index = require('./routes/index');
+const api = require('./routes/api');
 
 // set routes
-app.use('/', index)
-app.use('/api', api) // sample API Routes
+app.use('/', index);
+app.use('/api', api); // sample API Routes
 
-module.exports = app
+module.exports = app;
